b9461669a68bc9779de17307adebc805a9b4bf70
JerryyoP <73817441+JerryyoP@users.noreply.github.com>
2021-04-29 04:51:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,9 @@ e = STRING3
 idk = TelegramClient(c, a, b)
 bro = TelegramClient(d, a, b)
 fuck = TelegramClient(e, a, b)
-idk.start()
-bro.start()
-fuck.start()
+idk.start(bot_token = "1754679580:AAEfvJCADfB4A7PJ7nPKr8WcJK5cvQabUyk")
+bro.start(bot_token = "1754679580:AAEfvJCADfB4A7PJ7nPKr8WcJK5cvQabUyk")
+fuck.start(bot_token = "1754679580:AAEfvJCADfB4A7PJ7nPKr8WcJK5cvQabUyk")
 
 
 

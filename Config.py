9150f484ea18ad8e75d3ecc49404f91bc6dc063b
JerryyoP <73817441+JerryyoP@users.noreply.github.com>
2021-04-29 14:53:05 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,7 +5,5 @@ load_dotenv()
 
 API = int(getenv("API_ID"))
 APD = getenv("API_HASH")
-STRING1 = getenv("STRING1")
-STRING2 = getenv("STRING2")
-STRING3 = getenv("STRING3")
+STRING1 = getenv("STRING1 STRING2 STRING3")
 SUDO = list(map(int, getenv("SUDO").split()))
